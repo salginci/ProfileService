51c71111d98e46cbef773f3c1b7b4563daef56e4
salginci <ozgur@salginci.com>
2022-01-16 13:01:21 +0300

    Initial check-in of module ProfileService

---
+++ Controllers/PlatformUserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProfileService.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProfileService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlatformUserController : Controller
+    {
+        private readonly ILogger<PlatformUserController> _logger;
+
+        public  PlatformUserController(ILogger<PlatformUserController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet("{id}")]
+        public PlatformUser Get(string id)
+        {
+            return new PlatformUser { Fullname = "Ozgur SALGINCI", Id = "9090", Title = "Software Development Specialist" };
+
+
+        }
+
+
+
+    }
+}
---
+++ Models/PlarformUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ProfileService.Models
+{
+    public class PlatformUser
+    {
+        public PlatformUser()
+        {
+        }
+
+        public string Id { get; set; }
+        public string Fullname { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+
+    } 
+}
